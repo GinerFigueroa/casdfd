05d70f2de470c537bf64446605288da9b1ea35ab
Giner Figueroa <2113110108@untels.edu.pe>
2023-11-20 15:34:46 -0500

    Listo por Figueroa

--- proyectodjango/UC3_GutierrezOtazuFigueroa/miapp/views.py
+++ proyectodjango/UC3_GutierrezOtazuFigueroa/miapp/views.py
@@ -1,31 +1,63 @@
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.shortcuts import render, HttpResponse, redirect
-
+#-------------------------------------
+# Create your views here.
+layout = """
+    <h1> Proyecto Web (LP3) | RIVERA CASO </h1>
+    <hr/>
+    <ul>
+        <li>
+            <a href="/inicio"> INICIO</a>
+        </li>
+        <li>
+            <a href="/saludo"> MENSAJE DE SALUDO</a>
+        </li>
+        <li>
+            <a href="/rango"> MOSTRAR NÚMEROS [a,b]</a>
+        </li>
+        <li>
+            <a href="/rango2/10/40"> MOSTRAR NÚMEROS [10,15]</a>
+        </li>
+    </ul>
+    <hr/>
+"""
 
 def index(request):
     return render(request,'index.html')
 
-def inicios(request):
-    cursos = ['Gestion de base de datos', 'Dinamica de sistemas', 'LP3', 'Teoria general de sistemas', 'Algoritmo de computacion grafica', 'Gestion de procesos', 'Legislacion informatica']
-    return render(request, 'inicios.html', {'cursos': cursos})
+def saludo(request):
+    return render(request,'saludo.html')
+
+def rango(request):
+    a = 10
+    b = 20
+    resultado = f"""
+        <h2> Números de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul>
+    """
+
+    while a<=b:
+        resultado += f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
+
+def rango2(request,a=10,b=40):
+    if a>b:
+        return redirect('rango2',a=b, b=a)
+    resultado = f"""
+        <h2> Números de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul>
+    """
+    while a<=b:
+        resultado += f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
 
 
 
-def primitos(request,a=1,b=100):
-
-    if a > b:
-        a, b = b, a
-
-    primos = []
-
-    for num in range(a, b + 1):
-        if num > 1:
-            for i in range(2, num):
-                if (num % i) == 0:
-                    break
-            else:
-                primos.append(num)
-
-    return render(request, 'primos.html', {'primos': primos, 'a': a, 'b': b})
-
-def examen(request):
-    return render(request,'examen.html')
+#------------------------------------------------
